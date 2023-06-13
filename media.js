c435955f10849f2d648e4e321e6588fec4e18283
pedromujica1 <miottopedro@gmail.com>
2023-06-13 08:25:14 -0300

    bruhh

--- media.js
+++ media.js
@@ -1,7 +1,7 @@
 //criar referências ao elemento da pagina
 const frm = document.querySelector("form");
 const resp1 = document.querySelector("h3");
-const res2 = document.querySelector("h4")
+const res2 = document.querySelector("h4");
 
 frm.addEventListener("submit",(e) => {
    
@@ -18,9 +18,19 @@ frm.addEventListener("submit",(e) => {
 
     console.log(media);
 
+    name_display.innerText=`Aluno: ${nome}`
     resp1.innerText=`Média das Notas ${media.toFixed(2)}`;
-
-
-
-
+    
+    if (media >= 7) {
+        res2.innerText=`Parabéns ${nome}! Você foi aprovado(a)`;
+        res2.style.color = "blue"; 
+        
+    } else if(media >= 4){
+        res2.innerText = `Parabéns ${nome}! Você está de recuperação`;
+        res2.style.color="blue";
+    }
+    else{
+        res2.innerText = `Parabéns ${nome}! Você reprovou`;
+        res2.style.color="red";
+    }
 })
