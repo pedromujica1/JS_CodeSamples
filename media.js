be81a54d5704298e69c3f792422f8f174a929b93
pedromujica1 <miottopedro@gmail.com>
2023-06-13 07:59:33 -0300

    media js changed

---
+++ media.js
@@ -0,0 +1,12 @@
+//criar referências ao elemento da pagina
+const form = document.querySelector("form");
+const resp1 = document.querySelector("h3");
+const res2 = document.querySelector("h4")
+
+form.addEventListener("submit",(e) => {
+   
+    //Evista o envio do formulario
+    e.preventDefault();
+
+    
+})
