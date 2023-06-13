c9c0f7585dfdc579600437cb7a8ef89dea347265
pedromujica1 <miottopedro@gmail.com>
2023-06-13 08:10:28 -0300

    media is working

--- media.js
+++ media.js
@@ -1,12 +1,26 @@
 //criar referências ao elemento da pagina
-const form = document.querySelector("form");
+const frm = document.querySelector("form");
 const resp1 = document.querySelector("h3");
 const res2 = document.querySelector("h4")
 
-form.addEventListener("submit",(e) => {
+frm.addEventListener("submit",(e) => {
    
     //Evista o envio do formulario
     e.preventDefault();
 
-    
+    //Obter os valores digitados
+    const nome = frm.inNome.value;
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+
+    //Calculo da média
+    const media = (nota1+nota2)/2;
+
+    console.log(media);
+
+    resp1.innerText=`Média das Notas ${media.toFixed(2)}`;
+
+
+
+
 })
