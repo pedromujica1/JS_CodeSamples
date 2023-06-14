ed252170f1ea20fb496eed48a85f99ec48bcef9d
pedromujica1 <miottopedro@gmail.com>
2023-06-14 15:34:49 -0300

    parquimetro done

--- parquimetro/parquimetro.js
+++ parquimetro/parquimetro.js
@@ -3,50 +3,49 @@ const frm = document.querySelector("form");
 const tempo = document.querySelector("h3");
 const troco = document.querySelector("h4");
 
+//Se o valor for inferior ao tempo mínimo, exiba a mensagem: “Valor Insuficiente”. 
+//Valores: R$ 1,00 - 30 minutos
+//R$ 1.75 - 60 minutos 
+//R$3,00 - 120 minutos.
 frm.addEventListener("submit",(e) => {
    
     //Evita o envio do formulario
     e.preventDefault();
 
     //Obter os valores digitados
-    const lado1 = Number(frm.invalorA.value)
-    const lado2 = Number(frm.invalorB.value)
-    const lado3 = Number(frm.invalorC.value)
-
-    //if se o trinaglo existir
-    if (lado3 < (lado1+lado2) && lado2 < (lado1+lado3) && lado1 < (lado2+lado3)) 
+    const moedas = Number(frm.inMoedas.value)
+    const troco_30min = moedas - 1
+    const troco_60min = moedas - 1.75
+    const troco_120min = moedas - 3
+    
+    if (moedas < 1) 
     {
-        resultado.innerText = `O triângulo pode ser formado!`;
-        //cor gold
-        resultado.style.color="#FFD700";
-        
-        //
-        if (lado1 == lado2 && lado2 == lado3) 
-        {
-            
-            tipo_triangulo.innerText=`O triângulo é equilátero`
-            tipo_triangulo.style.color="red"
-
-        } 
-        
-        else if (lado1 == lado2 || lado1== lado3 || lado3 == lado2) 
-        
-        {
-            tipo_triangulo.innerText=`O triângulo é isósceles`
-            tipo_triangulo.style.color="blue"
-            
-        } else 
-        {
-            tipo_triangulo.innerText=`O triângulo é escaleno`
-            tipo_triangulo.style.color="green"
-        }
+        tempo.innerText=`Valor R$ ${moedas.toFixed(2)} insuficiente!`
     } 
     //Caso o triangulo não exista imprimir a mensagem a seguir
     else 
     {
-        resultado.innerText = `O triângulo não pode ser formado...`;
-        //cor indigo
-        resultado.style.color="#4B0082";
+        if (moedas => 1 && (moedas < 1.75)) 
+        {
+            
+            tempo.innerText=`Valor R$ ${moedas.toFixed(2)} suficiente, 30 minutos`
+            
+            troco.innerText=`Valor do troco ${troco_30min.toFixed(2)}`
+        }
+        if (moedas => 1.75 && (moedas < 3)) 
+        {
+            
+            tempo.innerText=`Valor R$ ${moedas.toFixed(2)} suficiente, 60 minutos`
+            
+            troco.innerText=`Valor do troco ${troco_60min.toFixed(2)}`
+        } 
+        if (moedas >= 3) 
+        {
+            
+            tempo.innerText=`Valor R$ ${moedas.toFixed(2)} suficiente, 120 minutos`
+            
+            troco.innerText=`Valor do troco ${troco_120min.toFixed(2)}`
+        }  
     }
     
 })
