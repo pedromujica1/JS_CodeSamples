4d50ec17cf5c4afe2e65f38962e2b0021890c3ec
pedromujica1 <miottopedro@gmail.com>
2023-06-14 15:10:58 -0300

    starting js parquimetro

---
+++ parquimetro/parquimetro.js
@@ -0,0 +1,52 @@
+//cria referências ao elemento da pagina
+const frm = document.querySelector("form");
+const tempo = document.querySelector("h3");
+const troco = document.querySelector("h4");
+
+frm.addEventListener("submit",(e) => {
+   
+    //Evita o envio do formulario
+    e.preventDefault();
+
+    //Obter os valores digitados
+    const lado1 = Number(frm.invalorA.value)
+    const lado2 = Number(frm.invalorB.value)
+    const lado3 = Number(frm.invalorC.value)
+
+    //if se o trinaglo existir
+    if (lado3 < (lado1+lado2) && lado2 < (lado1+lado3) && lado1 < (lado2+lado3)) 
+    {
+        resultado.innerText = `O triângulo pode ser formado!`;
+        //cor gold
+        resultado.style.color="#FFD700";
+        
+        //
+        if (lado1 == lado2 && lado2 == lado3) 
+        {
+            
+            tipo_triangulo.innerText=`O triângulo é equilátero`
+            tipo_triangulo.style.color="red"
+
+        } 
+        
+        else if (lado1 == lado2 || lado1== lado3 || lado3 == lado2) 
+        
+        {
+            tipo_triangulo.innerText=`O triângulo é isósceles`
+            tipo_triangulo.style.color="blue"
+            
+        } else 
+        {
+            tipo_triangulo.innerText=`O triângulo é escaleno`
+            tipo_triangulo.style.color="green"
+        }
+    } 
+    //Caso o triangulo não exista imprimir a mensagem a seguir
+    else 
+    {
+        resultado.innerText = `O triângulo não pode ser formado...`;
+        //cor indigo
+        resultado.style.color="#4B0082";
+    }
+    
+})
